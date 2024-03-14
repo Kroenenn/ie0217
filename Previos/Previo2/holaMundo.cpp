78f20765265ad9f1dd5e7007a9340bbb22924e39
Kroenenn <gabrielps200325@gmail.com>
2024-03-14 17:40:33 -0600

    Tercera parte del Previo2
    Signed-off-by: Kroenenn <gabrielps200325@gmail.com>

--- Previos/Previo2/holaMundo.cpp
+++ Previos/Previo2/holaMundo.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << "Hola a todos los de ie0217!\n";
     std::cout << "Archivo Previo 2\n";
 
-    int var1 = 2;
-    //int var2 = 2;
-    //int var3 = 3;
-
-    switch (var1) {
-        case 1:
-            std::cout << "Caso 1\n";
-            break;
-        case 2:
-        case 3:
-            std::cout << "Caso Final\n";
-            break;
-        default:
-            std::cout << "Caso default\n";
-            break;
-    }
-
+    std::cout << "argc: " << argc << std::endl;
+    std::cout << "argv[0]: " << argv[0] << std::endl;
+    std::cout << "argv[1]: " << argv[1] << std::endl;
+    std::cout << "argv[2]: " << argv[2] << std::endl;
+    std::cout << "argv[3]: " << argv[3] << std::endl;
     return 0;
 }
 
