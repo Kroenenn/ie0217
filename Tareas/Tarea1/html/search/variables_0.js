0d89c69803a53a85b03c42d0185d6d83809a922b
Kroenenn <gabrielps200325@gmail.com>
2024-04-01 17:23:51 -0600

    Actualizando el html
    Signed-off-by: Kroenenn <gabrielps200325@gmail.com>

--- Tareas/Tarea1/html/search/variables_0.js
+++ Tareas/Tarea1/html/search/variables_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['diccionario_32',['diccionario',['../struct_ahorcado.html#afd349639cae55062b7a286385a0d027e',1,'Ahorcado']]]
+  ['diccionario_32',['diccionario',['../struct_ahorcado.html#ad887dfe76c494ec9fd9cf27a2ada52c0',1,'Ahorcado']]]
 ];
