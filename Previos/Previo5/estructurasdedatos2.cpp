01b74f489113d1725dd71dcac3256a6c0bba4ff0
Kroenenn <gabrielps200325@gmail.com>
2024-04-08 12:26:38 -0600

    Agregando comentarios no guardados de estructurasdedatos2.cpp
    Signed-off-by: Kroenenn <gabrielps200325@gmail.com>

--- Previos/Previo5/estructurasdedatos2.cpp
+++ Previos/Previo5/estructurasdedatos2.cpp
@@ -23,19 +23,19 @@ int main() {
     three = new Node();
 
     // Asignar valores de value
-    one->value = 1;
-    two->value = 2;
-    three->value = 3;
+    one->value = 1; // Esto lo ue hace es asignar el valor 1 al campo value del nodo one.
+    two->value = 2; // Esto lo ue hace es asignar el valor 2 al campo value del nodo two.
+    three->value = 3; // Esto lo ue hace es asignar el valor 3 al campo value del nodo three.
 
     // Conectar nodos
-    one->next = two;
-    two->next = three;
-    three->next = NULL;
+    one->next = two; // Esto lo que hace es asignar el puntero two al campo next del nodo one.
+    two->next = three; // Esto lo que hace es asignar el puntero three al campo next del nodo two.
+    three->next = NULL; // Esto lo que hace es asignar NULL al campo next del nodo three.
 
     // Imprimir el valor de la lista enlazada
-    head = one;
-    while (head != NULL) {
-        cout << head->value;
-        head = head->next;
+    head = one; // Esto lo que hace es asignar el puntero one al puntero head.
+    while (head != NULL) { // Esto lo que hace es recorrer la lista enlazada e imprimir el valor de cada nodo.
+        cout << head->value; // Imprimir el valor del nodo
+        head = head->next; // Asignar el siguiente nodo a head
     }
 }
