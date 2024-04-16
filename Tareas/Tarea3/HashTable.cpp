8cf44b3fc6133ed801010f56d5f4435908b8d0a9
Kroenenn <gabrielps200325@gmail.com>
2024-04-16 14:55:14 -0600

    Agregando Bosquejo de HashTable.cpp y HashTable.hpp
    Signed-off-by: Kroenenn <gabrielps200325@gmail.com>

---
+++ Tareas/Tarea3/HashTable.cpp
@@ -0,0 +1,43 @@
+// HashTable.cpp
+
+#include "HashTable.hpp"
+#include <iostream>
+
+
+HashTable::HashTable() {
+
+}
+
+HashTable::~HashTable() {
+
+}
+
+int HashTable::funcionHash(std::string clave) {
+
+}
+
+void HashTable::agregarContacto(std::string nombre, std::string telefono) {
+
+}
+
+bool HashTable::eliminarContacto(std::string nombre, bool eliminarDeMemoria) {
+
+    
+}
+
+Contacto* HashTable::buscarContacto(std::string nombre) {
+
+}
+
+void HashTable::imprimirHashTable() {
+
+}
+
+void HashTable::mostrarTodosLosContactos() {
+
+}
+
+void HashTable::fusionarYOrdenarListas(ListaContactos& listaMaestra) {
+
+}
+
